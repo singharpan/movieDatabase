5a7dec865fbc263f0f2d0fee3872d67342c40897
Bucky <singharpan09@gmail.com>
2020-04-24 23:09:22 +0530

    Form Default Behavior

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        {/* <Movies /> */}
         <LoginForm />
       </main>
     );
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
-//ByDefault HTML forms takes a trip to the server :)
-
 class LoginForm extends Component {
+  //ByDefault HTML forms takes a trip to the server :)
+  //this handlesubmit takes event object as a parameter & the event object have a method "preventDefault"
+  //this prevent the form default behavior which is "submit to server" and "page reload"
+  //will now call the server,save the changes to server and redirect user to next page.
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   render() {
     return (
       <React.Fragment>
         <h1>Login Form</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
